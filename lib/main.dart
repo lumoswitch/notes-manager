6daff858153096be3acee6e4cf8096c6285bd5b1
lumoswitch <sofrona.anna@gmail.com>
2022-04-06 01:06:04 +0300

    notes vies created

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_manager/constants/routes.dart';
 import 'package:notes_manager/views/login_view.dart';
+import 'package:notes_manager/views/notes_view.dart';
 import 'package:notes_manager/views/register_view.dart';
 import 'package:notes_manager/views/verify_email_view.dart';
 
@@ -9,14 +11,15 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Notes Manager',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
       routes: {
-        '/login': (context) => const LoginView(),
-        '/register': (context) => const RegisterView(),
+        loginRoute : (context) => const LoginView(),
+        registerRoute : (context) => const RegisterView(),
+        verifyEmailRoute : (context) => const VerifyEmailView(),
       },
     ),
   );
@@ -35,14 +38,13 @@ class HomePage extends StatelessWidget {
             final user = FirebaseAuth.instance.currentUser;
             if (user != null) {
               if (user.emailVerified) {
-                print('Email is verified');
+                return const NotesView();
               } else {
                 return const VerifyEmailView();
               }
             } else {
               return const LoginView();
             }
-            return const Text('Done');
           default:
             return const CircularProgressIndicator();
         }
