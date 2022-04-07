ca185453afbf3b5ca24074937c0046996a7a7990
lumoswitch <sofrona.anna@gmail.com>
2022-04-07 14:13:57 +0300

    allow only verified user to login

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -59,10 +59,20 @@ class _LoginViewState extends State<LoginView> {
                         email: email,
                         password: password,
                       );
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                        dashboardRoute,
-                        (route) => false,
-                      );
+                      final user = FirebaseAuth.instance.currentUser;
+                      if (user?.emailVerified ?? false) {
+                        // user's email is verified
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                          dashboardRoute,
+                          (route) => false,
+                        );
+                      } else {
+                        // user's email is NOT verified
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                          verifyEmailRoute,
+                          (route) => false,
+                        );
+                      }
                     } on FirebaseAuthException catch (e) {
                       if (e.code == 'user-not-found') {
                         await showErrorDialog(
